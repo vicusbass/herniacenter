7ec12be3d5868c9c24214b4b32283afca59e59a6
Vasile Pop <vasile.pop@gmail.com>
2025-09-26 22:23:58 +0300

    add hernias

---
+++ src/config/contact.ts
@@ -0,0 +1,9 @@
+const phone = '0740387931';
+
+export default {
+  phone,
+  email: 'contact@herniacenter.ro',
+  whatsapp: `https://wa.me/4${phone}`,
+  facebook: 'https://www.facebook.com/herniacenter',
+  instagram: 'https://www.instagram.com/herniacenter',
+};
