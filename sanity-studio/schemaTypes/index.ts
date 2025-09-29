24f5796a6e39d2ed98a416082af98bc4ea5e54e1
Vasile Pop <vasile.pop@gmail.com>
2025-09-29 12:40:13 +0300

    added intervention model, links

--- sanity-studio/schemaTypes/index.ts
+++ sanity-studio/schemaTypes/index.ts
@@ -1,5 +1,6 @@
 import blockContent from './blockContent'
+import intervention from './intervention'
 import post from './post'
 import review from './review'
 
-export const schemaTypes = [post, blockContent, review]
+export const schemaTypes = [post, blockContent, review, intervention]
