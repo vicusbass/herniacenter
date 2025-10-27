b48ab4be995136b53d6a095a9753644f2fd1af97
Vasile Pop <vasile.pop@gmail.com>
2025-10-27 08:53:40 +0200

    add hernia model

---
+++ sanity-studio/schemaTypes/hernia.ts
@@ -0,0 +1,34 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'hernia',
+  title: 'Hernia',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 128,
+      },
+    }),
+    defineField({
+      name: 'body',
+      title: 'Body',
+      type: 'blockContent',
+    }),
+  ],
+
+  preview: {
+    select: {
+      title: 'title',
+    },
+  },
+})
